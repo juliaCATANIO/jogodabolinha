09b30642013d3618df0262bc0407d28ff774a3cd
juliaCATANIO <catanio.julia@escola.pr.gov.br>
2024-10-30 15:21:41 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,17 @@
-let xBolinha=300;
-let yBolinha=200;
-let diametroBolinha=20;
-let raio=diametroBolinha/2;
-let velocidadexBolinha=2;
-let velocidadeyBolinha=2;
-
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(0);
-  circle(xBolinha, yBolinha, diametroBolinha);
-  xBolinha+=velocidadexBolinha;
-  yBolinha+=velocidadeyBolinha;
-if (xBolinha>width||xBolinha<0){
-    velocidadexBolinha*=-1;}
-if (yBolinha>height||yBolinha<0){
-    velocidadeyBolinha*=-1;} 
+  background("white")
+             
+  stroke("blue");
+  fill("red");
+  
+  console.log (mouseIsPressed)
+  rect(mouseX,mouseX,20,35);
+  
+
+  
+  
+}
